dc418c2cc3770e7abbc134dbb7f6b55f53f46fa0
JakePy <101231593+jakepy@users.noreply.github.com>
2022-08-06 22:23:25 -0700

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,5 +1,3 @@
-from atexit import register
-from audioop import reverse
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
